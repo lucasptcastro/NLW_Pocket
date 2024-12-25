bd51d31716a6876ea0152800c6d8c3f499c575f1
lucasptcastro <lucasptcastro@gmail.com>
2024-12-25 17:08:54 -0300

    ✨ feat: added GestureHandlerRootView

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,20 @@
 // ARQUIVO DE CONFIGURAÇÃO DAS ROTAS DA APLICAÇÃO
 
 import { Stack } from "expo-router"
-import { useFonts, Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold } from "@expo-google-fonts/rubik"
+import {
+  useFonts,
+  Rubik_600SemiBold,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik"
 
 import { colors } from "@/styles/colors"
 
 import { Loading } from "@/components/Loading"
+import { StatusBar } from "expo-status-bar"
+import { View } from "react-native"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -20,13 +29,18 @@ export default function Layout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: {
-          backgroundColor: colors.gray[100],
-        },
-      }}
-    />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: colors.gray[100],
+            },
+          }}
+        />
+        <StatusBar translucent />
+      </View>
+    </GestureHandlerRootView>
   )
 }
