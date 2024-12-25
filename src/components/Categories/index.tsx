2cb2097e4ecbf18b45c4b212139186b5f4caa87e
lucasptcastro <lucasptcastro@gmail.com>
2024-12-25 17:08:28 -0300

    ✨ feat: created component

---
+++ src/components/Categories/index.tsx
@@ -0,0 +1,36 @@
+import { FlatList, View } from "react-native"
+
+import { Category } from "../Category"
+import { s } from "./styles"
+
+export type CategoriesProps = {
+  id: string
+  name: string
+}[]
+
+type Props = {
+  data: CategoriesProps
+  selected: string
+  onSelected: (id: string) => void
+}
+
+export function Categories({ data, selected, onSelected }: Props) {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        <Category
+          name={item.name}
+          iconId={item.id}
+          onPress={() => onSelected(item.id)}
+          isSelected={item.id === selected}
+        />
+      )}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={s.content}
+      style={s.container}
+    />
+  )
+}
